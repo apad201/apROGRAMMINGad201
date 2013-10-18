d0f36af5ed8c3c912c1ef9b07f0907e850b24cf9
apad201 <aram.t.l@icloud.com>
2013-10-18 00:44:39 +0000

    Magic

--- WeatherProgram.py
+++ WeatherProgram.py
@@ -3,22 +3,23 @@ import re
 import time
 
 filebuffer = ''
-i = 0
 while True:
     i = 0
     pi = subprocess.Popen(['./WeatherReport2.sh'],stdout=subprocess.PIPE)
-    for line in iter(pi.stdout.readline, ''):    
-     #   print line.rstrip()
+    for line in iter(pi.stdout.readline, ''):
+        print(line.decode("utf-8"))
         if i==1:
-            match = re.search(r'(\s+)(-?\d+)', line)
-            print match.group(2)
-            filebuffer = match.group(2)
+            temp = re.search('Images(\s+)(-?\d+).+F', line.decode("utf-8"))
+        #    print(temp.group(2))
+        if i==4:   
+            humidity = re.search('Humidity:(\s+)(\d+)', line.decode("utf-8"))
+        #    print(humidity.group(2))
         i = i+1
-        
-    print filebuffer
-    Myfile = open('TempOut.csv', 'a')
-    Myfile.write(filebuffer + ',')
+
+    filebuffer = temp.group(2) + ',' + humidity.group(2)    
+    #print(filebuffer)
+    Myfile = open('NewTempOut.csv', 'a')
+    Myfile.write(filebuffer + '\n')
     Myfile.close()
-    time.sleep(900)
-    
+    time.sleep(86400)
     
