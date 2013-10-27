83517cd5edd65ee1b88404d9e6654574936df7c4
apad201 <aram.t.l@icloud.com>
2013-10-27 20:53:14 +0000

    Add natural time reading to SmartWeather.py

--- SmartWeather.py
+++ SmartWeather.py
@@ -1,19 +1,25 @@
 import subprocess
 import re
 import time
+from datetime import datetime, timedelta
+
+
+    
 
 while True:
-    ts = time.time()
+    utc_dt = time.time()
     subprocess.call(['./durhamWeather2.sh'])
     time.sleep(2)
     readFile = open('weatherDump.txt', 'r')
-    writeFile = open('TempOut2.csv', 'a')
+    writeFile = open('TempOut3.csv', 'a')
     for line in readFile:
         match = re.search('Images\s+(-?\d+)', line)
         if match is not None:
             print(match.group(1))
-            writeFile.write('\n' + str(ts) + ',' + match.group(1))
+            today = datetime.today()
+            writeFile.write('\n' + str(utc_dt) + ',' + today.strftime('%H:%M on %b %d %Y') + ',' + match.group(1))
             break
     readFile.close()
     writeFile.close()
     time.sleep(3600)
+ 
